2cc10b69fea91fabd5ab5ef7f361474da1b3fbf4
Daniel Gonzalez <dntigrex@gmail.com>
2019-03-07 19:05:39 +0100

    holap

--- Master/beautifulSoup.py
+++ Master/beautifulSoup.py
@@ -28,12 +28,11 @@ if __name__ == "__main__":
     soup = beautifulRead(html_doc)
     for li in soup.find_all("li",attrs={"class": "threadbit"}):
         print(li.find(attrs={"class": "title"}).get("title"))
-        """print(li.find(attrs={"class": "threadstats td alt"}))
-        print("--------------------------------------------------------------------")"""
         for m in li.find(attrs={"class":"threadstats td alt"}).find_all("li"):
             print(m.get_text())
         print("--------------------------------------------------------------------")
         #print(li.find(attrs={"class": "threadstats td alt"}))
+        
         
         
        
