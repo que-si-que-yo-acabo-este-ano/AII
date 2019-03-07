40ea2ec95830e28aa6442b0139d2960425bb216b
Jose <pepelu101996@gmail.com>
2019-03-07 19:04:59 +0100

    Quitar comentario

--- Master/beautifulSoup.py
+++ Master/beautifulSoup.py
@@ -28,8 +28,6 @@ if __name__ == "__main__":
     soup = beautifulRead(html_doc)
     for li in soup.find_all("li",attrs={"class": "threadbit"}):
         print(li.find(attrs={"class": "title"}).get("title"))
-        """print(li.find(attrs={"class": "threadstats td alt"}))
-        print("--------------------------------------------------------------------")"""
         for m in li.find(attrs={"class":"threadstats td alt"}).find_all("li"):
             print(m.get_text())
         print("--------------------------------------------------------------------")
