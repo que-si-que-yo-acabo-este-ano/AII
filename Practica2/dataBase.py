a783c32b8b80b3a0d791e944768a0ed27e67adb5
Jose <pepelu101996@gmail.com>
2019-03-11 08:57:46 +0100

    Inicio BD

--- Practica2/dataBase.py
+++ Practica2/dataBase.py
@@ -2,18 +2,22 @@
 import sqlite3
 
 def startDataBase():
-    conn = sqlite3.connect('test.db')
+    conn = sqlite3.connect('ulalox.db')
     conn.text_factory = str  # para evitar problemas con el conjunto de caracteres que maneja la BD
     # conn.execute("DROP TABLE IF EXISTS FORUM")   
-    conn.execute('''CREATE TABLE IF NOT EXISTS FORUM
+    conn.execute('''CREATE TABLE IF NOT EXISTS PRODUCT
        (ID INTEGER PRIMARY KEY  AUTOINCREMENT,
-       TITLE        TEXT       NOT NULL,
-       LINK         TEXT       NOT NULL,
-       AUTHOR       TEXT       NOT NULL,
-       DATE         TEXT       NOT NULL,
-       ANSWERS      INTEGER    NOT NULL,
-       VISITS       INTEGER    NOT NULL);''')
-    conn.close()
+       MARCA          TEXT       NOT NULL,
+       NOMBRE         TEXT       NOT NULL,
+       LINK           TEXT       NOT NULL,
+       PRECIO         DOUBLE       NOT NULL,
+       PRECIO_OFERTA      INTEGER);''')
+    
 
 def insertDataBase():
-    pass
+    conn = sqlite3.connect('ulalox.db')
+    conn.execute("""INSERT INTO FORUM (MARCA,NOMBRE,LINK,PRECIO,PRECIO_OFERTA) VALUES ("Gluton","Artg","www.gluten.com",2.0,null)""")
+    conn.commit()
+    conn.close()    
+
+startDataBase()
