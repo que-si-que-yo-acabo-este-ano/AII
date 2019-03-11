04ff2e689761c332dc8a00d0da078bfade632a12
Daniel Gonzalez <dntigrex@gmail.com>
2019-03-11 10:21:17 +0100

    soup prat2

--- Practica2/beautifulSoup.py
+++ Practica2/beautifulSoup.py
@@ -4,6 +4,9 @@ import dataBase
 import urllib.request, re
 import os.path
 from bs4 import BeautifulSoup
+from idlelib.iomenu import encoding
+from decimal import Decimal
+from test.test_functools import decimal
 
 
 def open_url(url,file):
@@ -23,35 +26,48 @@ def beautifulRead(html):
     return BeautifulSoup(html,"html.parser")
 
 
-if __name__ == "__main__":
-    file="foros"
-    open_url("https://foros.derecho.com/foro/20-Derecho-Civil-General",file)
-    html_doc = open(file,"r")
+def lecturaWeb():
+    file="singluten"
+    open_url("https://www.ulabox.com/campaign/productos-sin-gluten#gref",file)
+    html_doc = open(file,"r",encoding="utf-8")
     soup = beautifulRead(html_doc)
-    linkRaiz = str(soup.find("base").get("href"))
-    infPag = []
-    for li in soup.find_all("li",attrs={"class": "threadbit"}):
-        titulo = li.find(attrs={"class": "title"}).get("title")
-        link = linkRaiz + str(li.find(attrs={"class": "title"}).get("href"))
-        autor = li.find(attrs={"class": "username understate"}).get_text()
-        fechaHora = str(li.find(attrs={"class":"label"}).get_text().split(",")[1]).lstrip()
-        fechaHora = fechaHora.replace("\xa0", "-")
-        respvis = []
-        for m in li.find(attrs={"class":"threadstats td alt"}).find_all("li",limit=2):
-            respvis.append(m.get_text()[-1])
+    listaFinal = []
+    for li in soup.find_all("div",attrs={"class": "grid__item m-one-whole t-one-third d-one-third dw-one-quarter | js-product-grid-grid"}):
+        #print(li)
+        url = li.find(attrs={"class": "product-item__main"}).find(attrs={"class": "product-item__image nauru js-pjax js-article-link"}).get("href")
+        marca = li.find(attrs={"class": "product-item__main"}).find(attrs={"class": "product-item__title"}).find(attrs="product-item__brand micro | push-half--bottom").get_text().lstrip()
+        marca = marca.rstrip(' ')
+        marca = marca.rstrip("\n")  
+        nombre = li.find(attrs={"class": "product-item__main"}).find(attrs={"class": "product-item__title"}).find(attrs="product-item__name zeta face-normal | flush--bottom").find("a").get_text().lstrip()
+        nombre = nombre.rstrip(' ')
+        nombre = nombre.rstrip("\n")
+        print(marca)
         
-        #TODO insertar en bd
-        print("Título:",titulo)
-        print("Link:",link)
-        print("Autor:",autor)
-        print("Fecha y Hora:",fechaHora)
-        #Posición 0 respuestas
-        print("Respuestas:",respvis[0])
-        #Posición 1 visitas
-        print("Visitas:",respvis[1])
-        tema = [titulo,link,autor,fechaHora,int(respvis[0]),int(respvis[1])]
-        print(tema)
-        print("--------------------------------------------------------------------")
+        precio = li.find(attrs={"class": "delta"}).get_text()
+        precio2 = li.find(attrs={"class": "milli"}).get_text()
+        precioFinal = (precio + precio2[:-2]).replace(",",".")
+        precioFinal = Decimal(precioFinal)
+#         print(precio)
+#         print(precio2)
+        lista=[]
+        oferta = li.find(attrs={"class": "product-item__price product-item__price--old product-grid-footer__price--old nano | flush--bottom"})
+        if(oferta!=None):
+            oferta = str(oferta).replace("<del class=\"product-item__price product-item__price--old product-grid-footer__price--old nano | flush--bottom\">","")[:-8]   
+            lista.append(marca)
+            lista.append(nombre)
+            lista.append(oferta)
+            lista.append(precioFinal)
+        else:
+            lista.append(marca)
+            lista.append(nombre)
+            lista.append(precioFinal)
+            lista.append(None)
+            
+        listaFinal.append(lista)
+    return listaFinal
+
+print(lecturaWeb())
+       
         
     #dataBase.startDataBase()
         
