ec08e2c2850a2eebcddd9b8659f6013e84ef22e0
Jose <pepelu101996@gmail.com>
2019-03-11 09:28:20 +0100

    a y c hechos

--- Practica2/dataBase.py
+++ Practica2/dataBase.py
@@ -16,17 +16,31 @@ def startDataBase():
 
 def insertDataBase(productos):
     conn = sqlite3.connect('ulalox.db')
-    conn.execute("""INSERT INTO PRODUCTO (MARCA,NOMBRE,LINK,PRECIO) VALUES ('Gluton','Artg','www.gluten.com',2.0)""")
+    for producto in productos:
+        conn.execute("""INSERT INTO PRODUCTO (MARCA,NOMBRE,LINK,PRECIO,PRECIO_OFERTA) VALUES (?,?,?,?,?)""",(producto[0],producto[1],producto[2],producto[3],producto[4]))
     conn.commit()
     conn.close()    
 
-def selectDataBase():
+def selectDataBaseMarca(marca):
     conn = sqlite3.connect('ulalox.db')
-    rows = conn.execute("""SELECT * FROM PRODUCTO WHERE MARCA='Gluton'""")
+    rows = conn.execute("""SELECT * FROM PRODUCTO""")
+    res = []
     for producto in rows.fetchall():
-        print(producto)
+        res.append(producto)
     conn.close()
+    return res
     
+def selectDataBaseOfertas():
+    conn = sqlite3.connect('ulalox.db')
+    rows = conn.execute("""SELECT NOMBRE,PRECIO,PRECIO_OFERTA FROM PRODUCTO WHERE PRECIO_OFERTA is not null""")
+    res = []
+    for producto in rows.fetchall():
+        res.append(producto)
+    conn.close()
+    return res
+
+productosPrueba = [['Gluton','Artg','www.gluten.com',2.0,None],['Gluton22323','Artg','www.gluten.com',2.0,1.0]]
 startDataBase()
-insertDataBase([])
-selectDataBase()
+insertDataBase(productosPrueba)
+#selectDataBaseMarca()
+print(selectDataBaseOfertas())
