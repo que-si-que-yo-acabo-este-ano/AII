8472ca392fd9a8c67f84117127abc4ae8a46957d
Jose <pepelu101996@gmail.com>
2019-03-12 14:00:24 +0100

    Beauti Finished

--- Ejercicio1-BeautifulSoup/beautifulSoup.py
+++ Ejercicio1-BeautifulSoup/beautifulSoup.py
@@ -7,16 +7,17 @@ from idlelib.iomenu import encoding
 
 
 def startDataBase():
-    conn = sqlite3.connect('ulalox.db')
+    conn = sqlite3.connect('diario.db')
     conn.text_factory = str  # para evitar problemas con el conjunto de caracteres que maneja la BD
-    conn.execute("DROP TABLE IF EXISTS DIARIO")   
+    conn.execute("DROP TABLE IF EXISTS DIARIO")
     conn.execute('''CREATE TABLE IF NOT EXISTS DIARIO
        (ID INTEGER PRIMARY KEY  AUTOINCREMENT,
-       MARCA          TEXT       NOT NULL,
-       NOMBRE         TEXT       NOT NULL,
-       LINK           TEXT       NOT NULL,
-       PRECIO         DOUBLE     NOT NULL,
-       PRECIO_OFERTA  DOUBLE);''')
+       JORNADA            INTEGER       NOT NULL,
+       LOCAL              TEXT       NOT NULL,
+       VISITANTE          TEXT       NOT NULL,
+       GOLES_LOCALES      INTEGER       NOT NULL,
+       GOLES_VISITANTES   INTEGER     NOT NULL,
+       LINK               TEXT NOT NULL);''')
     conn.close()
 
 def open_url(url,file):
@@ -36,7 +37,25 @@ def lecturaWeb():
     open_url("http://resultados.as.com/resultados/futbol/primera/2017_2018/calendario/",file)
     html_doc = open(file,"r",encoding="utf-8")
     soup = beautifulRead(html_doc)
-    
+    jornadas = []
+    i = 1
+    for jornada in soup.find_all("tbody"): #jornadas
+        jornadaBD = []
+        for partido in jornada.find_all("tr"):
+            #equipos print(partido.find("span",attrs={"class","nombre-equipo"}).get_text())
+            local = partido.find("td",attrs={"class","col-equipo-local"}).find("span",attrs={"class","nombre-equipo"}).get_text()
+            visitante = partido.find("td",attrs={"class","col-equipo-visitante"}).find("span",attrs={"class","nombre-equipo"}).get_text()
+            resultado = partido.find("td",attrs={"class","col-resultado"}).find("a").get_text().split()
+            golesLocales = resultado[0]
+            golesVisitantes = resultado[2]
+            link = partido.find("td",attrs={"class","col-resultado"}).find("a").get("href")
+            #print(local,int(golesLocales),"-",int(golesVisitantes),visitante,link)
+            jornadaBD.append([i,local,visitante,int(golesLocales),int(golesVisitantes),link])
+        jornadas.append(jornadaBD)
+        i+=1
+    print(jornadas)
+
+lecturaWeb()
 
 # for x in lecturaWeb():
 #     print(x)
