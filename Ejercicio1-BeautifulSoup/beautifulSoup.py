e9508ba07855bd591da6db7d6f272a9b8a3e6f22
Jose <pepelu101996@gmail.com>
2019-03-12 16:16:03 +0100

    beauti finished for second exercise

--- Ejercicio1-BeautifulSoup/beautifulSoup.py
+++ Ejercicio1-BeautifulSoup/beautifulSoup.py
@@ -4,6 +4,7 @@ import urllib.request, re
 import sqlite3
 from bs4 import BeautifulSoup
 from idlelib.iomenu import encoding
+from numpy import insert
 
 
 def startDataBase():
@@ -19,6 +20,36 @@ def startDataBase():
        GOLES_VISITANTES   INTEGER     NOT NULL,
        LINK               TEXT NOT NULL);''')
     conn.close()
+    
+def insertDataBase(jornadas):
+    conn = sqlite3.connect('diario.db')
+    for jornada in jornadas:
+        conn.execute("""INSERT INTO DIARIO (JORNADA,LOCAL,VISITANTE,GOLES_LOCALES,GOLES_VISITANTES,LINK) VALUES (?,?,?,?,?,?)""",(jornada[0],jornada[1],jornada[2],jornada[3],jornada[4],jornada[5]))
+    conn.commit()
+    conn.close()   
+
+def selectDataBaseJornadas():
+    conn = sqlite3.connect('diario.db')
+    rows = conn.execute("""SELECT JORNADA,LOCAL,VISITANTE,GOLES_LOCALES,GOLES_VISITANTES,LINK FROM DIARIO""")
+    res = []
+    for jornada in rows.fetchall():
+        res.append(jornada)
+    conn.close()
+    return res 
+
+def selectDataBasePartidosPorJornada(jornada):
+    conn = sqlite3.connect('diario.db')
+    rows = conn.execute("""SELECT JORNADA,LOCAL,VISITANTE,GOLES_LOCALES,GOLES_VISITANTES,LINK FROM DIARIO WHERE JORNADA='{0}'""".format(jornada))
+    res = []
+    for jornada in rows.fetchall():
+        res.append(jornada)
+    conn.close()
+    return res 
+
+def selectDataBaseGoles(jornada,local,visitante):
+    conn = sqlite3.connect('diario.db')
+    row = conn.execute("""SELECT LINK FROM DIARIO WHERE JORNADA='{0}' AND LOCAL='{1}' AND VISITANTE='{2}'""".format(jornada,local,visitante))
+    return row.fetchall()
 
 def open_url(url,file):
     try:
@@ -40,7 +71,6 @@ def lecturaWeb():
     jornadas = []
     i = 1
     for jornada in soup.find_all("tbody"): #jornadas
-        jornadaBD = []
         for partido in jornada.find_all("tr"):
             #equipos print(partido.find("span",attrs={"class","nombre-equipo"}).get_text())
             local = partido.find("td",attrs={"class","col-equipo-local"}).find("span",attrs={"class","nombre-equipo"}).get_text()
@@ -50,18 +80,41 @@ def lecturaWeb():
             golesVisitantes = resultado[2]
             link = partido.find("td",attrs={"class","col-resultado"}).find("a").get("href")
             #print(local,int(golesLocales),"-",int(golesVisitantes),visitante,link)
-            jornadaBD.append([i,local,visitante,int(golesLocales),int(golesVisitantes),link])
-        jornadas.append(jornadaBD)
+            jornadas.append([i,local,visitante,int(golesLocales),int(golesVisitantes),link])
         i+=1
-    print(jornadas)
+    return jornadas
 
-lecturaWeb()
-
-# for x in lecturaWeb():
-#     print(x)
-# print(lecturaWeb())
+def lecturaWebGoles(jornada,local,visitante):
+    raiz = "https://resultados.as.com"
+    path = selectDataBaseGoles(jornada, local, visitante)[0][0]
+    link = raiz + path
+    file = str(jornada)+"-"+local+"vs"+visitante
+    open_url(link,file)
+    html_doc = open(file,"r",encoding="utf-8")
+    soup = beautifulRead(html_doc)
+    goles = []
+    for evento in soup.find_all("p",attrs={"class","txt-accion"}):
+        accion = evento.find("span",attrs={"class","hidden-xs"}).get_text()
+        if accion == "Gol":
+            minuto = evento.find("span",attrs={"class","min-evento"}).get_text()
+            jugador = evento.find("strong").get_text()
+            goles.append([minuto,jugador])
+    return goles
         
-    #dataBase.startDataBase()
+    #poner str(jornada) si desde tkinter no entra un string
+    
+
+"""
+startDataBase()
+insertDataBase(lecturaWeb())
+#a
+print(len(selectDataBaseJornadas()))
+#b
+print(selectDataBaseJornadas())
+#a 2º ejer
+print(selectDataBasePartidosPorJornada(3))"""
+
+lecturaWebGoles(1,"Sevilla","Espanyol")
         
         
        
