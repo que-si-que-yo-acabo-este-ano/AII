519fb3fd78eb2f4e363c1972418aa1229a495dab
Jose <pepelu101996@gmail.com>
2019-05-13 08:54:47 +0200

    Modelos creados

--- django_evaluable/principal/admin.py
+++ django_evaluable/principal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from principal.models import Usuario,Diario,Autor,Noticia
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Diario)
+admin.site.register(Autor)
+admin.site.register(Noticia)
--- django_evaluable/principal/models.py
+++ django_evaluable/principal/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Diario(models.Model):
+    nombre = models.CharField(max_length=100,primary_key=True)
+    pais = models.CharField(max_length=20)
+    idioma = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.nombre
+
+class Usuario(models.Model):
+    nombreUsuario = models.CharField(max_length=20,primary_key=True)
+    passwd = models.CharField(max_length=10)
+    email = models.EmailField()
+    nombre = models.CharField(max_length=20)
+    apellidos = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.nombreUsuario
+    
+class Autor(models.Model):
+    nombre = models.CharField(max_length=20,primary_key=True)
+    apellidos = models.CharField(max_length=20) 
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.nombre
+    
+class Noticia(models.Model):
+    titular = models.CharField(max_length=100,primary_key=True)
+    fechaNoticia = models.DateField()
+    resumen = models.TextField()
+    tipos = (('Deportes','Deportes'),('Cultura','Cultura'),('Politica','Politica'),('Economia','Economia'),('Actualidad','Actualidad'))
+    tipoNoticia = models.CharField(max_length=15,choices=tipos)
+    diario = models.ForeignKey("Diario",on_delete=models.CASCADE)
+    autores = models.ManyToManyField("Autor")
+    usuarios = models.ManyToManyField("Usuario")
+    
+    
+    
