93b679cc7f6e94f059514616ae68a25d6bf285a7
Diego <reloth94@hotmail.com>
2019-05-27 10:00:58 +0200

    topTags of artist

--- Django_contenido/main/views.py
+++ Django_contenido/main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from main.models import Artista, UsuarioEtiquetaArtista
+from collections import Counter
 
 # Create your views here.
+
+
+
+
+
+
+def artistTopTagsByUsers(artist):
+    artistSearched = Artista.objects.get(nombre= artist)
+    allTagsObj = UsuarioEtiquetaArtista.objects.filter(artista= artistSearched)
+    allTags = [obj.etiqueta for obj in allTagsObj]
+    top4Tuples = Counter(allTags).most_common(4)
+    print(top4Tuples)
+    return top4Tuples    
